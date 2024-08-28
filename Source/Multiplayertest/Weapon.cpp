9928b1440bdcef2cfad6e63cc5b013cf90e4e81c
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-08-28 13:48:26 -0400

    Revert "Weapon pickup widget with functionality ready for use"
    This reverts commit bc755135dc1c29f8fb591eaab5a83a1712bb63c4.

--- Source/Multiplayertest/Weapon.cpp
+++ Source/Multiplayertest/Weapon.cpp
@@ -3,8 +3,6 @@
 
 #include "Weapon.h"
 #include "Components/SphereComponent.h"
-#include "Components/WidgetComponent.h"
-#include "Character/ShooterPlayer.h"
 
 AWeapon::AWeapon()
 {
@@ -23,37 +21,18 @@ AWeapon::AWeapon()
 	AreaSphere->SetupAttachment(RootComponent);
 	AreaSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	AreaSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	
-	PickupWidget = CreateDefaultSubobject <UWidgetComponent>(TEXT("PickupWidget"));
-	PickupWidget->SetupAttachment(RootComponent);
 }
 
 void AWeapon::BeginPlay()
 {
 	Super::BeginPlay();
 
-
 	if (HasAuthority()) 
 	{
 		AreaSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 		AreaSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
-		AreaSphere->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnSphereOverlap);
-	}
-	if (PickupWidget)
-	{
-		PickupWidget->SetVisibility(false);
 	}
 	
-}
-
-void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	AShooterPlayer* ShooterCharacter = Cast<AShooterPlayer>(OtherActor);
-
-	if(ShooterCharacter && PickupWidget)
-	{
-		PickupWidget->SetVisibility(true);
-	}
 }
 
 void AWeapon::Tick(float DeltaTime)
--- Source/Multiplayertest/Weapon.h
+++ Source/Multiplayertest/Weapon.h
@@ -27,16 +27,6 @@ public:
 	virtual void Tick(float DeltaTime) override;
 protected:
 	virtual void BeginPlay() override;
-
-	UFUNCTION()
-	virtual void OnSphereOverlap(
-		UPrimitiveComponent* OverlappedComponent,
-		AActor* OtherActor,
-		UPrimitiveComponent* OtherComp,
-		int32 OtherBodyIndex,
-		bool bFromSweep,
-		const FHitResult& SweepResult
-	);
 private:
 	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
 	USkeletalMeshComponent* WeaponMesh;
@@ -46,9 +36,6 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	EWeaponState WeaponState;
-	
-	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
-	class UWidgetComponent* PickupWidget;
 public:	
 
 };
