c40ba7117d0a60889d111cecd1a6769b134d6d90
Juan Jose Vergara <40677444+jjvemz@users.noreply.github.com>
2024-08-28 13:52:46 -0400

    Reapply "Weapon pickup widget with functionality ready for use"
    This reverts commit 9928b1440bdcef2cfad6e63cc5b013cf90e4e81c.

--- Source/Multiplayertest/Weapon.h
+++ Source/Multiplayertest/Weapon.h
@@ -27,6 +27,16 @@ public:
 	virtual void Tick(float DeltaTime) override;
 protected:
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnSphereOverlap(
+		UPrimitiveComponent* OverlappedComponent,
+		AActor* OtherActor,
+		UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult& SweepResult
+	);
 private:
 	UPROPERTY(VisibleAnywhere, Category="Weapon Properties")
 	USkeletalMeshComponent* WeaponMesh;
@@ -36,6 +46,9 @@ private:
 
 	UPROPERTY(VisibleAnywhere)
 	EWeaponState WeaponState;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Weapon Properties")
+	class UWidgetComponent* PickupWidget;
 public:	
 
 };
